364f8965ccbf6590880cd90707a971e96f9374e8
Vilson Fabricio Juliatto <fabricio@juliatto.com>
2022-01-29 02:27:56 -0300

    test: add 404 integration test

--- tests/main.rs
+++ tests/main.rs
@@ -19,3 +19,22 @@ async fn health_check_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
+
+#[tokio::test]
+async fn non_existing_route_returns_404() {
+    let application = Application::new("127.0.0.1", 0).unwrap();
+    let port = application.port();
+    tokio::spawn(async {
+        application.start(false).await.unwrap();
+    });
+
+    let client = Client::new();
+    let response = client
+        .get(&format!("http://127.0.0.1:{}/i_do_not_exist", port))
+        .send()
+        .await
+        .expect("Failed to execute query");
+
+    assert_eq!(response.status(), 404);
+    assert_eq!(Some(0), response.content_length());
+}
