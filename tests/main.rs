ed8a65d2592aa59401dd27b6a91f4f81188193b6
Vilson Fabricio Juliatto <fabricio@juliatto.com>
2022-01-29 02:21:06 -0300

    test: add integration test for health check

---
+++ tests/main.rs
@@ -0,0 +1,21 @@
+use atto_chat_server::application::Application;
+use reqwest::Client;
+
+#[tokio::test]
+async fn health_check_works() {
+    let application = Application::new("127.0.0.1", 0).unwrap();
+    let port = application.port();
+    tokio::spawn(async {
+        application.start().await.unwrap();
+    });
+
+    let client = Client::new();
+    let response = client
+        .get(&format!("http://127.0.0.1:{}/health_check", port))
+        .send()
+        .await
+        .expect("Failed to execute query");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
